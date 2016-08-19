8818a435d80d6bc2206bb99ca313f61a4bbad6a1
Vinayak Kankanwadi <email>
2016-08-20 06:14:00 +1000

    add service and controller into site javascript module

--- html-module-controller-service/site.js
+++ html-module-controller-service/site.js
@@ -1,1 +1,9 @@
-var module = angular.module('siteapp', []);
+var module = angular.module('siteApp', []);
+
+module.service('SiteService',function() {
+    console.log('Site Service created!');
+});
+
+module.controller('SiteController',function(){
+	console.log('Site Controller created!');
+});
