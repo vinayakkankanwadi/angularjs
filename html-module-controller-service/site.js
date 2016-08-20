c3bed49c179337c951d98fbf41aa3d47b824d394
Vinayak Kankanwadi <email>
2016-08-20 10:28:34 +1000

    clean up

--- html-module-controller-service/site.js
+++ html-module-controller-service/site.js
@@ -3,9 +3,6 @@ var module = angular.module('siteApp', []);
 module.service('siteService',function() {
     console.log('Site Service created!');
 
-    //to create unique site id
-    var uid = 1;
-	
 	//Site array to hold list of all sites
     var sites = [{
         'id': '1001-2001',
@@ -56,9 +53,9 @@ module.service('siteService',function() {
     //iterate through sites list and delete 
     //site if found
     this.delete = function (id) {
-        for (i in sites) {
-            if (sites[i].id == id) {
-                sites.splice(i, 1);
+        for (i in keys) {
+            if (keys[i].id == id) {
+                keys[i].key = '';
             }
         }
     }
@@ -69,40 +66,22 @@ module.service('siteService',function() {
         //for existing site, find this site using id
         //and update it
 		for (i in keys) {
-			if (keys[i].id == id) {
-				keys[i].key = 'XAE';
+			if (keys[i].id == id && keys[i].key == '') {
+				keys[i].key = this.getRandomSpan();
             }
 		}
     }	
 	
-    //save method create a new site if not already exists
-    //else update the existing object
-    this.save = function (site) {
-        if (site.id == null) {
-            //if this is new site, add it in sites array
-            site.id = uid++;
-            sites.push(site);
-        } else {
-            //for existing site, find this site using id
-            //and update it.
-            for (i in sites) {
-                if (sites[i].id == site.id) {
-                    sites[i] = site;
-                }
-            }
-        }
-    }
+	this.getRandomSpan = function(){
+		return Math.floor((Math.random()*6)+1);
+	}
+	
 });
 
 module.controller('siteController',function($scope, siteService){
 	console.log('Site Controller created!');
 
     $scope.sites = siteService.list();	
-	
-    $scope.saveSite = function () {
-        siteService.save($scope.newsite);
-        $scope.newsite = {};
-    }
 	
     $scope.generate = function (id) {
         siteService.generate(id);
@@ -111,13 +90,9 @@ module.controller('siteController',function($scope, siteService){
 	
     $scope.delete = function (id) {
         siteService.delete(id);
-        if ($scope.newsite.id == id) $scope.newsite = {};
+		$scope.update(id);
     }
 
-    $scope.edit = function (id) {
-        $scope.newsite = angular.copy(siteService.get(id));
-    }
-    
 	$scope.update = function (id) {
         $scope.currentsite = angular.copy(siteService.getSite(id));
 		$scope.currentkey = angular.copy(siteService.getKey(id));
