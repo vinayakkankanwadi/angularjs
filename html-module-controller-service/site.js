9a28afb654df2d23673022e0ee5f5a1304c56aed
Vinayak Kankanwadi <email>
2016-08-20 10:10:59 +1000

    update for key asssign to site using select

--- html-module-controller-service/site.js
+++ html-module-controller-service/site.js
@@ -10,7 +10,23 @@ module.service('siteService',function() {
     var sites = [{
         'id': '1001-2001',
         'name': 'Orange'
-    }];
+    },
+	{
+	    'id': '1001-2003',
+        'name': 'Feres'	
+	}
+	];
+
+	//Key array to hold list of all keys
+    var keys = [{
+        'id': '1001-2001',
+        'key': '222aaa'
+    },
+	{
+	    'id': '1001-2003',
+        'key': ''	
+	}
+	];
 	
 	//simply returns the site list
     this.list = function () {
@@ -19,10 +35,20 @@ module.service('siteService',function() {
 	
     //simply search sites list for given id
     //and returns the site object if found
-    this.get = function (id) {
+    this.getSite = function (id) {
         for (i in sites) {
             if (sites[i].id == id) {
                 return sites[i];
+            }
+        }
+    }
+	
+    //simply search keys list for given id
+    //and returns the key object if found
+    this.getKey = function (id) {
+        for (i in keys) {
+            if (keys[i].id == id) {
+                return keys[i];
             }
         }
     }
@@ -36,6 +62,18 @@ module.service('siteService',function() {
             }
         }
     }
+
+    //generate method create a new key if not already exists
+    //else update the existing object
+    this.generate = function (id) {
+        //for existing site, find this site using id
+        //and update it
+		for (i in keys) {
+			if (keys[i].id == id) {
+				keys[i].key = 'XAE';
+            }
+		}
+    }	
 	
     //save method create a new site if not already exists
     //else update the existing object
@@ -53,7 +91,6 @@ module.service('siteService',function() {
                 }
             }
         }
-
     }
 });
 
@@ -67,13 +104,22 @@ module.controller('siteController',function($scope, siteService){
         $scope.newsite = {};
     }
 	
+    $scope.generate = function (id) {
+        siteService.generate(id);
+		$scope.update(id);
+    }
+	
     $scope.delete = function (id) {
-
         siteService.delete(id);
         if ($scope.newsite.id == id) $scope.newsite = {};
     }
 
     $scope.edit = function (id) {
         $scope.newsite = angular.copy(siteService.get(id));
+    }
+    
+	$scope.update = function (id) {
+        $scope.currentsite = angular.copy(siteService.getSite(id));
+		$scope.currentkey = angular.copy(siteService.getKey(id));
     }
 });
